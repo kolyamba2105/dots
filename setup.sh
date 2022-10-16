bbeea0ead3376a42fa38ef4c8ac06ed6220c9379
kolyamba2105 <haravetskyy@gmail.com>
2022-10-16 16:24:19 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -14,12 +14,6 @@ ln -s $PWD/.tmux.conf ~/.tmux.conf
 rm ~/.gitconfig
 ln -s $PWD/.gitconfig ~/.gitconfig
 
-rm ~/Brewfile
-ln -s $PWD/Brewfile ~/Brewfile
-
-rm ~/Brewfile.lock.json
-ln -s $PWD/Brewfile.lock.json ~/Brewfile.lock.json
-
 rm -rf ~/.config/alacritty
 ln -s $PWD/.config/alacritty ~/.config/alacritty
 
