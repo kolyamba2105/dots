e38ee7e246d908a77c0ab9e2f826c19e0800c54b
kolyamba2105 <haravetskyy@gmail.com>
2023-03-13 12:50:34 +0200

    Adjust update.sh

--- update.sh
+++ update.sh
@@ -1,15 +1,22 @@
 #!/bin/sh
 
-rm Brewfile
-rm Brewfile.lock.json
+if [ $# -eq 0 ]; then
+  echo "Error: No argument provided"
+  exit 1
+elif [ "$1" != "a" ] && [ "$1" != "b" ]; then
+  echo "Error: Argument must be 'a' or 'b'"
+  exit 1
+fi
+
+dir="brew/$1"
 
 brew update
 brew upgrade
 
 brew cleanup --prune=all
 
-brew bundle dump
-brew bundle install
+brew bundle dump --force --file=$dir/Brewfile
+brew bundle install --file=$dir/Brewfile
 
-git add Brewfile Brewfile.lock.json
-git commit -m "Update Brewfile"
+git add $dir/Brewfile $dir/Brewfile.lock.json
+git commit -m "Update Brewfile [$1]"
