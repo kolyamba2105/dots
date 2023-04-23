f8bf23c0c3dc83872e3f88530534dbd0ff7e9c7f
kolyamba2105 <haravetskyy@gmail.com>
2023-04-23 17:39:06 +0300

    Update wezterm config

--- .wezterm.lua
+++ .wezterm.lua
@@ -4,7 +4,7 @@ return {
   color_scheme = "Catppuccin Mocha",
   font = wezterm.font("JetBrainsMono Nerd Font", { weight = 500 }),
   font_size = 14,
+  native_macos_fullscreen_mode = true,
   use_fancy_tab_bar = false,
-  window_decorations = "RESIZE",
   window_padding = { bottom = "0.75cell", left = "0.75cell", right = "0.75cell", top = "0.75cell" },
 }
