59adec1c76f1e2a5516aee0eaefc06b0a1e80b62
kolyamba2105 <haravetskyy@gmail.com>
2023-12-29 12:27:54 +0200

    wezterm - Adjust opacity/blur settings

--- .wezterm.lua
+++ .wezterm.lua
@@ -4,10 +4,10 @@ return {
   color_scheme = "Catppuccin Mocha",
   font = wezterm.font("JetBrainsMono NF", { weight = 500 }),
   font_size = 12,
-  macos_window_background_blur = 50, -- Looks better when window_background_opacity < 1
+  macos_window_background_blur = 75, -- Looks better when window_background_opacity < 1
   native_macos_fullscreen_mode = true,
   use_fancy_tab_bar = false,
-  window_background_opacity = 0.9, -- Fixes MacOS animation lag
+  window_background_opacity = 0.95, -- Fixes MacOS animation lag
   window_decorations = "RESIZE",
   window_padding = { bottom = "0.75cell", left = "0.75cell", right = "0.75cell", top = "0.75cell" },
 }
