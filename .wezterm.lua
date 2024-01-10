9e516004be1db68c3336d1acc6ea34c8b4693ce3
kolyamba2105 <haravetskyy@gmail.com>
2024-01-10 15:09:18 +0200

    Increase font size in wezterm

--- .wezterm.lua
+++ .wezterm.lua
@@ -3,7 +3,7 @@ local wezterm = require("wezterm")
 return {
   color_scheme = "Catppuccin Mocha",
   font = wezterm.font("JetBrainsMono NF", { weight = 500 }),
-  font_size = 12,
+  font_size = 14,
   macos_window_background_blur = 75, -- Looks better when window_background_opacity < 1
   native_macos_fullscreen_mode = true,
   use_fancy_tab_bar = false,
