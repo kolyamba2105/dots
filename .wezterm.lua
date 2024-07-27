9bfa3570dfdc10e2b329479ebdc5c97f0e53725d
kolyamba2105 <haravetskyy@gmail.com>
2024-07-27 12:04:03 +0300

    Use config_builder for WezTerm config

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,12 +1,14 @@
 local wezterm = require("wezterm")
 
-return {
-  color_scheme = "Catppuccin Mocha",
-  font = wezterm.font("BlexMono Nerd Font", { weight = 500 }),
-  font_size = 16,
-  native_macos_fullscreen_mode = true,
-  use_fancy_tab_bar = false,
-  window_background_opacity = 0.999, -- Fixes MacOS animation lag
-  window_decorations = "RESIZE",
-  window_padding = { bottom = "0.75cell", left = "0.75cell", right = "0.75cell", top = "0.75cell" },
-}
+local config = wezterm.config_builder()
+
+config.color_scheme = "Catppuccin Mocha"
+config.font = wezterm.font("BlexMono Nerd Font", { weight = 500 })
+config.font_size = 16
+config.native_macos_fullscreen_mode = true
+config.use_fancy_tab_bar = false
+config.window_background_opacity = 0.999 -- Fixes MacOS animation lag
+config.window_decorations = "RESIZE"
+config.window_padding = { bottom = "0.75cell", left = "0.75cell", right = "0.75cell", top = "0.75cell" }
+
+return config
