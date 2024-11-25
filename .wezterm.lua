5293ffa9dc64130c2ecf03e8ee9c229f3a8aa026
kolyamba2105 <haravetskyy@gmail.com>
2024-11-26 00:52:27 +0200

    wezterm - Set max_fps to 240

--- .wezterm.lua
+++ .wezterm.lua
@@ -6,6 +6,7 @@ config.color_scheme = "Catppuccin Mocha"
 config.font = wezterm.font("JetBrainsMono Nerd Font", { weight = 600 })
 config.font_size = 14
 config.hide_tab_bar_if_only_one_tab = true
+config.max_fps = 240
 config.native_macos_fullscreen_mode = true
 config.use_fancy_tab_bar = false
 config.window_background_opacity = 0.999 -- Fixes MacOS animation lag
